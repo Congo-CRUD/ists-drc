891b378d38182d8689a8727271a5164c1a76a23d
LeGrandMAG <magmukendi0@gmail.com>
2022-12-31 20:58:37 +0900

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,21 +3,37 @@ import { windowSize } from '../../utils'
 import React, { useState } from 'react'
 
 
+function OpenMiniNavBar(){
+    return(
+        <div className='mini-nav-open'>
+            <ul>
+                <li><a>Accueil</a></li>
+                <li><a>A propos</a></li>
+                <li><a>Sections Organisées</a></li>
+                <li><a>Contact</a></li>
+            </ul>
+        </div>
+    )
+}
+
 function MiniNavBar(){
+    const [navIsOpen, setNavIsOpen] = useState(false); 
     return(
         <nav className='mini-nav'>
-            HI
+            {navIsOpen && <OpenMiniNavBar/>}
+            <i class="fa-solid fa-bars fa-xl"></i>
         </nav>
     )
 }
 
 function BigNavBar(){
     return(
-    <nav className='nav'></nav>
+    <nav className='nav'>Big</nav>
         
     )
 }
 function Navbar() {
+
     const size = windowSize(window.innerWidth) 
     return (
         size === "mobile" ? <MiniNavBar/> : <BigNavBar/>
